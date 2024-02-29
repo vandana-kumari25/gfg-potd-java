ab9901b22d9d4a0386fb763dbade41776a65ae0e
Vandana Kumari <124623856+vandana-kumari25@users.noreply.github.com>
2024-02-29 23:32:43 +0530

    29-02-2024

---
+++ February-2024/29-02-2024.java
@@ -0,0 +1,59 @@
+/*
+Author : Vandana Kumari 
+Date : 29/02/2024
+Problem : Sum of bit differences
+Problem Link : https://www.geeksforgeeks.org/problems/sum-of-bit-differences2937/1
+*/
+
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine());
+        while (tc-- > 0) {
+            int n = Integer.parseInt(br.readLine());
+            int[] arr = new int[n];
+            String[] inputLine = br.readLine().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            System.out.println(new Solution().sumBitDifferences(arr, n));
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+
+class Solution 
+{
+    long sumBitDifferences(int[] arr, int n) 
+    {
+        // code here
+        long ans = 0;
+        for (int i = 0; i < 32; i++) 
+        {
+            long count = 0;
+            
+            for (int j = 0; j < n; j++)
+                if ((arr[j] & (1 << i)) != 0)
+                    count++;
+                    
+            ans += (count * (n - count) * 2);
+        }
+        return ans;
+    }
+}
