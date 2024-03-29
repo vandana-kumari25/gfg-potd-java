4031436df368c20655b92e7951cbd6a95cd16929
Vandana Kumari <124623856+vandana-kumari25@users.noreply.github.com>
2024-03-29 20:07:06 +0530

    29-03-2024

---
+++ March-2024/29-03-2024.java
@@ -0,0 +1,56 @@
+/*
+Author : Vandana Kumari 
+Date : 29/03/2024
+Problem : Euler Circuit in an Undirected Graph
+Problem Link : https://www.geeksforgeeks.org/problems/euler-circuit-in-a-directed-graph/1
+*/
+
+
+//{ Driver Code Starts
+    // Initial Template for Java
+    
+    import java.io.*;
+    import java.lang.*;
+    import java.util.*;
+    
+    class GFG {
+        public static void main(String[] args) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            int T = Integer.parseInt(br.readLine().trim());
+            while (T-- > 0) {
+                String[] s = br.readLine().trim().split(" ");
+                int V = Integer.parseInt(s[0]);
+                int E = Integer.parseInt(s[1]);
+                ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+                for (int i = 0; i < V; i++) adj.add(i, new ArrayList<Integer>());
+                for (int i = 0; i < E; i++) {
+                    String[] S = br.readLine().trim().split(" ");
+                    int u = Integer.parseInt(S[0]);
+                    int v = Integer.parseInt(S[1]);
+                    adj.get(u).add(v);
+                    adj.get(v).add(u);
+                }
+                Solution obj = new Solution();
+                boolean ans = obj.isEularCircuitExist(V, adj);
+                if (ans)
+                    System.out.println("1");
+                else
+                    System.out.println("0");
+            }
+        }
+    }
+    // } Driver Code Ends
+    
+    
+    // User function Template for Java
+    
+    class Solution {
+        public boolean isEularCircuitExist(int v, ArrayList<ArrayList<Integer>> adj) {
+            // Code here
+            for (int i = 0; i < v; i++) {
+                if (adj.get(i).size() % 2 == 1)
+                    return false;
+            }
+            return true;
+        }
+    }
