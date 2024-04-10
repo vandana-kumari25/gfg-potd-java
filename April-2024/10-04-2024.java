a63fc20fd42f286128d7da9547776cf256687382
Vandana Kumari <124623856+vandana-kumari25@users.noreply.github.com>
2024-04-10 21:18:38 +0530

    10-04-2024

---
+++ April-2024/10-04-2024.java
@@ -0,0 +1,48 @@
+/*
+Author : Vandana Kumari 
+Date : 10/04/2024
+Problem : Party of Couples
+Problem Link : https://www.geeksforgeeks.org/problems/alone-in-couple5507/1
+*/
+
+//{ Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+            int N = Integer.parseInt(read.readLine());
+            String input_line[] = read.readLine().trim().split("\\s+");
+            int arr[]= new int[N];
+            for(int i = 0; i < N; i++)
+                arr[i] = Integer.parseInt(input_line[i]);
+            
+            Solution ob = new Solution();
+            System.out.println(ob.findSingle(N, arr));
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    static int findSingle(int n, int arr[]){
+        // code here
+        int xor=0;
+        for(int ele:arr)
+        {
+            xor^=ele;
+        }
+        return xor;
+    }
+}
