217c9590a83f779bc176e6e5a9b219ab1a913d3c
Vandana Kumari <124623856+vandana-kumari25@users.noreply.github.com>
2024-04-20 21:20:12 +0530

    20-04-2024

---
+++ April-2024/20-04-2024.java
@@ -0,0 +1,80 @@
+/*
+Author : Vandana Kumari 
+Date : 20/04/2024
+Problem : Union of Two Sorted Arrays
+Problem Link : https://www.geeksforgeeks.org/problems/union-of-two-sorted-arrays-1587115621/1
+*/
+
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Main
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t  =Integer.parseInt(read.readLine());
+        
+        while(t-- > 0)
+        {
+            String st[] = read.readLine().trim().split(" ");
+            int N = Integer.parseInt(st[0]);
+            int M = Integer.parseInt(st[1]);
+            
+            int arr1[] = new int[N];
+            int arr2[] = new int[M];
+            
+            st = read.readLine().trim().split(" ");
+            for(int i = 0; i < N; i++)
+              arr1[i] = Integer.parseInt(st[i]);
+            
+            st = read.readLine().trim().split(" ");  
+            for(int i = 0; i< M; i++)
+              arr2[i] = Integer.parseInt(st[i]);
+            
+            Solution obj = new Solution();
+            ArrayList<Integer> res = new ArrayList<Integer>();
+            res = obj.findUnion(arr1, arr2, N, M);
+            for(int i = 0;i<res.size();i++)
+                System.out.print(res.get(i) + " ");
+            System.out.println(); 
+        }
+    }
+}
+
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+//arr1,arr2 : the arrays
+// n, m: size of arrays
+class Solution
+{
+    //Function to return a list containing the union of the two arrays.
+    public static ArrayList<Integer> findUnion(int arr1[], int arr2[], int n, int m)
+    {
+        // add your code here
+        HashSet<Integer> dataSet = new HashSet<>();
+        for(int z : arr1) {
+            dataSet.add(z);
+        }
+        for(int num : arr2) {
+            dataSet.add(num);
+        }
+        
+        ArrayList<Integer> result = new ArrayList<>(dataSet);
+        Collections.sort(result);
+        return result;
+    }
+}
+
+
+
