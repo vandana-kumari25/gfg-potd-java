2ceb35547c9562ccc4d5227d3bd35bc090139003
Vandana Kumari <124623856+vandana-kumari25@users.noreply.github.com>
2024-05-18 21:14:29 +0530

    18-05-2024

---
+++ May-2024/18-05-2024.java
@@ -0,0 +1,41 @@
+/*
+Author : Vandana Kumari 
+Date : 18/05/2024
+Problem : Find the Highest number
+Problem Link : https://www.geeksforgeeks.org/problems/find-the-highest-number2259/1
+*/
+
+//{ Driver Code Starts
+    import java.io.*;
+    import java.lang.*;
+    import java.util.*;
+    
+    class GFG {
+        public static void main(String[] args) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            int T = Integer.parseInt(br.readLine().trim());
+            while (T-- > 0) {
+                String s = br.readLine().trim();
+                int n = Integer.parseInt(s);
+                String S = br.readLine();
+                String[] s1 = S.split(" ");
+                List<Integer> a = new ArrayList<Integer>();
+                for (int i = 0; i < n; i++) {
+                    a.add(Integer.parseInt(s1[i]));
+                }
+                Solution ob = new Solution();
+                int ans = ob.findPeakElement(a);
+                System.out.println(ans);
+            }
+        }
+    }
+    
+    // } Driver Code Ends
+    
+    
+    class Solution {
+        public int findPeakElement(List<Integer> a) {
+            // Code here
+            return Collections.max(a);
+        }
+    }
