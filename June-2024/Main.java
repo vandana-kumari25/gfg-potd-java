9f6343f697e1a82135087ac2b795900c7ad0dfa8
Vandana Kumari <124623856+vandana-kumari25@users.noreply.github.com>
2024-06-10 11:15:41 +0530

    10-06-2024

---
+++ June-2024/Main.java
@@ -0,0 +1,57 @@
+/*
+Author : Vandana Kumari 
+Date : 10/06/2024
+Problem : Nuts and Bolts Problem
+Problem Link : https://www.geeksforgeeks.org/problems/nuts-and-bolts-problem0431/1
+*/
+
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            int n = Integer.parseInt(br.readLine().trim());
+            char[] nuts = new char[n], bolts = new char[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                nuts[i] = (inputLine[i].charAt(0));
+            }
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                bolts[i] = (inputLine[i].charAt(0));
+            }
+
+            new Solution().matchPairs(n, nuts, bolts);
+            for (int i = 0; i < n; i++) {
+                System.out.print(nuts[i] + " ");
+            }
+            System.out.println();
+            for (int i = 0; i < n; i++) {
+                System.out.print(bolts[i] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    void matchPairs(int n, char nuts[], char bolts[]) {
+        // code here
+        // Sort the nuts and bolts arrays
+        Arrays.sort(nuts);
+        Arrays.sort(bolts);
+    }
+}
