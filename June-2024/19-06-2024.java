ec9bd1fcac78de92d0b4a2a0f6f054004d1b1e69
Vandana Kumari <124623856+vandana-kumari25@users.noreply.github.com>
2024-06-19 21:26:19 +0530

    19-06-2024

---
+++ June-2024/19-06-2024.java
@@ -0,0 +1,44 @@
+/*
+Author : Vandana Kumari 
+Date : 19/06/2024
+Problem : Find maximum volume of a cuboid
+Problem Link : https://www.geeksforgeeks.org/problems/magical-box5306/1
+*/
+
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+
+        while (t-- > 0) {
+            String arr[] = read.readLine().trim().split("\\s+");
+            double perimeter = Double.parseDouble(arr[0]);
+            double area = Double.parseDouble(arr[1]);
+
+            Solution ob = new Solution();
+            double ans = ob.maxVolume(perimeter, area);
+            System.out.println(String.format("%.2f", ans));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+
+    double maxVolume(double perimeter, double area) {
+        // code here
+        double part1 = (perimeter - Math.sqrt(Math.pow(perimeter, 2) - (24 * area))) / 12;
+        double part2 = (perimeter / 4) - (2 * part1);
+        double ans = Math.pow(part1, 2) * part2;
+        return ans;
+    }
+}
