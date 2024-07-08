af7b843b6493813b6ccc343eab0df5091f3d3e3b
Vandana Kumari <124623856+vandana-kumari25@users.noreply.github.com>
2024-07-08 19:55:27 +0530

    08-07-2024

---
+++ July-2024/08-07-2024.java
@@ -0,0 +1,59 @@
+/*
+Author : Vandana Kumari 
+Date : 08/07/2024
+Problem : Search in Rotated Sorted Array
+Problem Link : https://www.geeksforgeeks.org/problems/search-in-a-rotated-array4618/1
+*/
+
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out = new PrintWriter(System.out);
+
+        int t = Integer.parseInt(in.readLine().trim());
+        while (t-- > 0) {
+            String line = in.readLine();
+            String[] tokens = line.split(" ");
+
+            // Create an ArrayList to store the integers
+            ArrayList<Integer> array = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                array.add(Integer.parseInt(token));
+            }
+
+            int[] arr = new int[array.size()];
+            int idx = 0;
+            for (int i : array) arr[idx++] = i;
+
+            int key = Integer.parseInt(in.readLine().trim());
+
+            out.println(new Solution().search(arr, key));
+        }
+        out.close();
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    int search(int[] arr, int key) {
+        // Complete this function
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == key) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
