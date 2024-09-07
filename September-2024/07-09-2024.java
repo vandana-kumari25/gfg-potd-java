5d175fb3fac6fad35ee697c0da477b3a9695e346
Vandana Kumari <124623856+vandana-kumari25@users.noreply.github.com>
2024-09-07 21:41:57 +0530

    07-09-2024

---
+++ September-2024/07-09-2024.java
@@ -0,0 +1,46 @@
+/*
+Author : Vandana Kumari 
+Date : 07/09/2024
+Problem : Nth Natural Number
+Problem Link : https://www.geeksforgeeks.org/problems/nth-natural-number/1
+*/
+
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner ob = new Scanner(System.in);
+        int t = ob.nextInt();
+        while (t-- > 0) {
+            long n = ob.nextLong();
+            Solution ab = new Solution();
+            long k = ab.findNth(n);
+            System.out.println(k);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    long findNth(long n) {
+        // code here
+        long base9num = 0;
+        long pos = 1;
+        
+        while (n > 0) {
+            base9num += (n % 9) * pos;
+            n /= 9;
+            pos *= 10;
+        }
+        
+        return base9num;
+    }
+}
