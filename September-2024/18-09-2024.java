cc92c9dfe8990a26be1c348273a40c89e1e3bbd0
Vandana Kumari <124623856+vandana-kumari25@users.noreply.github.com>
2024-09-22 14:26:22 +0530

    18-09-2024

---
+++ September-2024/18-09-2024.java
@@ -0,0 +1,68 @@
+/*
+Author : Vandana Kumari 
+Date : 18/09/2024
+Problem : Parenthesis Checker
+Problem Link : https://www.geeksforgeeks.org/problems/parenthesis-checker2744/1
+*/
+
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Driverclass
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        
+        //Reading total number of testcases
+        int t= sc.nextInt();
+        
+        while(t-- >0)
+        {
+            //reading the string
+            String st = sc.next();
+            
+            //calling ispar method of Paranthesis class 
+            //and printing "balanced" if it returns true
+            //else printing "not balanced"
+            if(new Solution().ispar(st) == true)
+                System.out.println("balanced");
+            else
+                System.out.println("not balanced");
+        
+        }
+    }
+}
+// } Driver Code Ends
+
+
+
+class Solution
+{
+    //Function to check if brackets are balanced or not.
+    static boolean ispar(String x)
+    {
+        // add your code here
+        Stack<Character> s = new Stack<>();
+        
+        for (char c : x.toCharArray()) {
+            switch(c) {
+                case '(': case '{': case '[':
+                    s.push(c);
+                    break;
+                case ')':
+                    if (s.isEmpty() || s.pop() != '(') return false;
+                    break;
+                case '}':
+                    if (s.isEmpty() || s.pop() != '{') return false;
+                    break;
+                case ']':
+                    if (s.isEmpty() || s.pop() != '[') return false;
+                    break;
+            }
+        }
+        return s.isEmpty();
+    }
+}
