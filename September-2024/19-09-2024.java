c986e5bf809fed30e72aa9fb02eb6350c0be41f9
Vandana Kumari <124623856+vandana-kumari25@users.noreply.github.com>
2024-09-22 14:22:56 +0530

    19-09-2024

---
+++ September-2024/19-09-2024.java
@@ -0,0 +1,49 @@
+/*
+Author : Vandana Kumari 
+Date : 20/09/2024
+Problem : Reverse Words
+Problem Link : https://www.geeksforgeeks.org/problems/reverse-words-in-a-given-string5459/1
+*/
+
+//{ Driver Code Starts
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t > 0) {
+            String s = sc.next();
+            Solution obj = new Solution();
+            System.out.println(obj.reverseWords(s));
+            t--;
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+
+class Solution {
+    // Function to reverse words in a given string.
+    String reverseWords(String str) {
+        // code here
+        StringBuilder result = new StringBuilder();
+        int n = str.length();
+        int end = n - 1;
+
+        for (int i = n - 1; i >= 0; i--) {
+            if (str.charAt(i) == '.') {
+                result.append(str.substring(i + 1, end + 1)).append(".");
+                end = i - 1; 
+            }
+        }
+        result.append(str.substring(0, end + 1));
+
+        return result.toString();
+    }
+}
